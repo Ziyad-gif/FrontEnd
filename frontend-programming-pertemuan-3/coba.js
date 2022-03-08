067f118fb6d5579eda7ccdbcc9f060655f864d4c
Your Name <you@example.com>
2022-03-08 15:13:08 +0700

    done

---
+++ frontend-programming-pertemuan-3/coba.js
@@ -0,0 +1,11 @@
+function sum(... numbers){
+    console.log(numbers);
+}
+
+sum(1, 2, 3, 4, 5);
+
+const names = ["Michael", "Hannah", "Jonas"];
+
+names.forEach(function(name){
+    console.log(name);
+})
